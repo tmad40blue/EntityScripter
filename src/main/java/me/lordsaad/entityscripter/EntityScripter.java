702f233548a4690f959d41ad8e03470ed9b6f624
Saad <saadodi44@gmail.com>
2016-01-23 20:11:16 +0200

    First Commit

---
+++ src/main/java/me/lordsaad/entityscripter/EntityScripter.java
@@ -0,0 +1,17 @@
+package me.lordsaad.entityscripter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Saad on 1/23/2016.
+ */
+public class EntityScripter extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
