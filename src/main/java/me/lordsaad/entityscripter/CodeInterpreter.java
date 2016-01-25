0c1acbb66c2397f4c8f1241eca9fa51fa7743c82
Saad <saadodi44@gmail.com>
2016-01-25 11:31:11 +0200

    added tons of crap

--- src/main/java/me/lordsaad/entityscripter/CodeInterpreter.java
+++ src/main/java/me/lordsaad/entityscripter/CodeInterpreter.java
@@ -1,25 +1,20 @@
 package me.lordsaad.entityscripter;
 
-import com.google.common.base.Joiner;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Created by Saad on 1/23/2016.
@@ -36,47 +31,86 @@ public class CodeInterpreter {
         this.file = file;
     }
 
-    public void interpretCode(Location loc) {
-        EntityBuilder builder = new EntityBuilder(loc);
+    public void interpretCode() {
+        EntityBuilder builder = new EntityBuilder();
         if (yml.contains("properties")) {
             for (String property : yml.getConfigurationSection("properties").getKeys(false)) {
-
-                if (property.equalsIgnoreCase("set_entity_type")) {
-                    builder.setEntityType(EntityType.valueOf(yml.getString("properties.set_entity_type").toUpperCase()));
-                }
-
-                if (property.equalsIgnoreCase("set_custom_name")) {
-                    builder.setEntityType(EntityType.valueOf(yml.getString("properties.set_custom_name").toUpperCase()));
-                }
-
-                if (property.equalsIgnoreCase("set_entity_type")) {
-                    builder.setEntityType(EntityType.valueOf(yml.getString("properties.set_entity_type").toUpperCase()));
-                }
-
-                if (property.equalsIgnoreCase("set_entity_type")) {
-                    builder.setEntityType(EntityType.valueOf(yml.getString("properties.set_entity_type").toUpperCase()));
-                }
-
+                matcher("properties.", property, builder);
             }
         }
-        builder.build();
+        builder.spawn();
     }
 
-    public void matcher(String key, Entity entity) {
-        if (key.equalsIgnoreCase("set_entity_type")) {
-            builder.setEntityType(EntityType.valueOf(yml.getString("properties.set_entity_type").toUpperCase()));
+    public void matcher(String prefixPath, String key, EntityBuilder builder) {
+
+        if (key.equalsIgnoreCase("set_entity_type"))
+            builder.setEntityType(EntityType.valueOf(yml.getString(prefixPath + "set_entity_type").toUpperCase()));
+
+        if (key.equalsIgnoreCase("set_custom_name"))
+            builder.setCustomName(yml.getString(prefixPath + "set_custom_name"));
+
+        if (key.equalsIgnoreCase("set_entity_name_visible"))
+            builder.setCustomNameVisible((yml.getBoolean(prefixPath + "set_custom_name_visible")));
+
+        if (key.equalsIgnoreCase("set_no_ai"))
+            builder.setNoAI(yml.getBoolean(prefixPath + "set_no_ai"));
+
+        if (key.equalsIgnoreCase("silent")) {
+            builder.setSilent(yml.getBoolean(prefixPath + "silent"));
         }
 
-        if (key.equalsIgnoreCase("set_custom_name")) {
-            builder.setEntityType(EntityType.valueOf(yml.getString("properties.set_custom_name").toUpperCase()));
+        if (key.equalsIgnoreCase("set_location")) {
+            for (String parameter : yml.getConfigurationSection(prefixPath + "set_location").getKeys(false)) {
+
+                double x = 0, y = 0, z = 0;
+                World world = null;
+                float pitch = 0, yaw = 0;
+
+                if (parameter.equalsIgnoreCase("x")) x = yml.getDouble(prefixPath + "set_location.x");
+                if (parameter.equalsIgnoreCase("y")) y = yml.getDouble(prefixPath + "set_location.y");
+                if (parameter.equalsIgnoreCase("z")) z = yml.getDouble(prefixPath + "set_location.z");
+                if (parameter.equalsIgnoreCase("world")) world = Bukkit.getWorld(yml.getString(prefixPath + "set_location.world"));
+                if (parameter.equalsIgnoreCase("pitch")) pitch = (float) yml.getDouble(prefixPath + "set_location.pitch");
+                if (parameter.equalsIgnoreCase("yaw")) yaw = (float) yml.getDouble(prefixPath + "set_location.yaw");
+
+                if (world != null) builder.setLocation(new Location(world, x, y, z, pitch, yaw));
+            }
         }
 
-        if (key.equalsIgnoreCase("set_entity_type")) {
-            builder.setEntityType(EntityType.valueOf(yml.getString("properties.set_entity_type").toUpperCase()));
+        if (key.equalsIgnoreCase("add_potion_effect")) {
+            for (String effectType : yml.getConfigurationSection(prefixPath + "add_potion_effect").getKeys(false)) {
+                PotionEffectType potionEffectType = PotionEffectType.getByName(effectType);
+
+                for (String parameter : yml.getConfigurationSection(prefixPath + "add_potion_effect." + potionEffectType).getKeys(false)) {
+
+                    int duration = 9999, amplifier = 1;
+                    boolean ambient = false, particles = true;
+
+                    if (parameter.equalsIgnoreCase("duration"))
+                        duration = yml.getInt(prefixPath + "add_potion_effect." + potionEffectType + ".duration");
+
+                    if (parameter.equalsIgnoreCase("amplifier"))
+                        amplifier = yml.getInt(prefixPath + "add_potion_effect." + potionEffectType + ".amplifier");
+
+                    if (parameter.equalsIgnoreCase("ambient"))
+                        ambient = yml.getBoolean(prefixPath + "add_potion_effect." + potionEffectType + ".ambient");
+
+                    if (parameter.equalsIgnoreCase("particles"))
+                        particles = yml.getBoolean(prefixPath + "add_potion_effect." + potionEffectType + ".particles");
+
+                    builder.addPotionEffect(new PotionEffect(potionEffectType, duration, amplifier, ambient, particles));
+                }
+            }
         }
 
-        if (key.equalsIgnoreCase("set_entity_type")) {
-            builder.setEntityType(EntityType.valueOf(yml.getString("properties.set_entity_type").toUpperCase()));
+        if (key.equalsIgnoreCase("custom_nbt")) {
+            for (String nbt : yml.getConfigurationSection(prefixPath + "custom_nbt").getKeys(false)) {
+                if (StringUtils.isNumeric(yml.getString(prefixPath + "custom_nbt" + nbt))) {
+                    builder.addCustomNBT(nbt, Integer.parseInt(yml.getString(prefixPath + "custom_nbt" + nbt)));
+                } else {
+                    builder.addCustomNBT(nbt, yml.getString(prefixPath + "custom_nbt" + nbt));
+                }
+            }
         }
     }
 }
--- src/main/java/me/lordsaad/entityscripter/EntityBuilder.java
+++ src/main/java/me/lordsaad/entityscripter/EntityBuilder.java
@@ -1,8 +1,15 @@
 package me.lordsaad.entityscripter;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.potion.PotionEffect;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Saad on 1/23/2016.
@@ -11,25 +18,50 @@ public class EntityBuilder {
 
     private EntityType entityType = EntityType.ZOMBIE;
     private String customName = null;
-    private boolean customNameVisible = false;
-    private boolean noAI = false;
-    private Location loc;
+    private boolean customNameVisible = false, noAI = false, silent = false;
+    private Location location = null;
+    private Entity entity;
+    private List<PotionEffect> potionEffects = new ArrayList<>();
+    private BiMap<String, Integer> customIntNBT = HashBiMap.create();
+    private BiMap<String, String> customStringNBT = HashBiMap.create();
 
-    public EntityBuilder(Location loc) {
-        this.loc = loc;
+    public EntityBuilder() {
     }
 
-    public Entity build() {
-        Entity entity = loc.getWorld().spawnEntity(loc, getEntityType());
+    public EntityBuilder(Entity entity) {
+        this.entity = entity;
+    }
 
+    public Entity spawn() {
+        if (location != null) {
+            Entity entity = location.getWorld().spawnEntity(location, getEntityType());
+            inject(entity);
+            return entity;
+
+        } else return null;
+    }
+
+    public void inject(Entity entity) {
         if (getCustomName() != null) entity.setCustomName(getCustomName());
 
         entity.setCustomNameVisible(getCustomNameVisible());
 
         if (getNoAI()) NBTUtils.addEntityTag(entity, "NoAI", 1);
 
+        if (!entity.getLocation().equals(location)) entity.teleport(location);
 
-        return entity;
+        if (entity instanceof LivingEntity) {
+            LivingEntity livingEntity = (LivingEntity) entity;
+            getPotionEffects().forEach(livingEntity::addPotionEffect);
+        }
+
+        for (String nbt : customIntNBT.keySet()) {
+            NBTUtils.addEntityTag(entity, nbt, customIntNBT.get(nbt));
+        }
+
+        for (String nbt : customStringNBT.keySet()) {
+            NBTUtils.addEntityTag(entity, nbt, customStringNBT.get(nbt));
+        }
     }
 
     public EntityType getEntityType() {
@@ -62,5 +94,49 @@ public class EntityBuilder {
 
     public void setNoAI(boolean noAI) {
         this.noAI = noAI;
+    }
+
+    public Entity getEntity() {
+        return entity;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public void addPotionEffect(PotionEffect potionEffect) {
+        this.potionEffects.add(potionEffect);
+    }
+
+    public List<PotionEffect> getPotionEffects() {
+        return potionEffects;
+    }
+
+    public boolean isSilent() {
+        return silent;
+    }
+
+    public void setSilent(boolean silent) {
+        this.silent = silent;
+    }
+
+    public void addCustomNBT(String nbt, String value) {
+        customStringNBT.put(nbt, value);
+    }
+
+    public void addCustomNBT(String nbt, int value) {
+        customIntNBT.put(nbt, value);
+    }
+
+    public BiMap<String, String> getCustomStringNBTs() {
+        return customStringNBT;
+    }
+
+    public BiMap<String, Integer> getCustomIntNBTs() {
+        return customIntNBT;
     }
 }
